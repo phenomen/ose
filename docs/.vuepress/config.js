518505d90b416ecfc92469a1a9b6b92fbb662c37
Alexander Vasilenko <phenomen@pm.me>
2019-08-29 10:50:57 -0500

    Index page revamp

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,35 +1,78 @@
 module.exports = {
-  docsDir: 'docs',
-  lang: 'ru-RU',
-  title: 'OSE',
-  description: 'Версия на русском языке',
-  themeConfig: {
-    nav: [
-      { text: 'Основы', link: '/core/' },
-      { text: 'Фэнтези', link: '/fantasy/' },
-      { text: 'Магия', link: '/magic/' },
-      { text: 'Монстры', link: '/monsters/' },
-      { text: 'Сокровища', link: '/treasures/' }
-    ],
-    sidebar: [
-      {
-        title: 'Классические правила',
-        children: [
-      '/core/',
-      '/fantasy/',
-      '/magic/',
-      '/monsters/',
-      '/treasures/'
-                  ]
-       },
-       {
-         title: 'Расширенные правила',
-         children: [
-       '/advanced-fantasy/',
-       '/advanced-magic/'
-                   ]
-        }
-
-     ]
-  }
+    docsDir: 'docs',
+    lang: 'ru-RU',
+    title: 'Old-School Essentials',
+    description: 'Версия на русском языке',
+    themeConfig: {
+        nav: [{
+                text: 'Основные правила',
+                link: '/core/'
+            },
+            {
+                text: 'Фэнтези',
+                link: '/fantasy/'
+            },
+            {
+                text: 'Магия',
+                link: '/magic/'
+            },
+            {
+                text: 'Монстры',
+                link: '/monsters/'
+            },
+            {
+                text: 'Сокровища',
+                link: '/treasures/'
+            }
+        ],
+        sidebar: [{
+                title: 'Основные правила',
+                children: [
+                    '/core/',
+                    '/core/introduction',
+                ]
+            },
+            {
+                title: 'Фэнтези',
+                children: [
+                    '/fantasy/'
+                ]
+            }
+            ,
+            {
+                title: 'Магия',
+                children: [
+                    '/magic/'
+                ]
+            }
+            ,
+            {
+                title: 'Монстры',
+                children: [
+                    '/monsters/'
+                ]
+            }
+            ,
+            {
+                title: 'Сокровища',
+                children: [
+                    '/treasures/'
+                ]
+            }
+            ,
+            {
+                title: 'Расширенное фэнтези',
+                children: [
+                    '/advanced-fantasy/'
+                ]
+            }
+            ,
+            {
+                title: 'Расширенная магия',
+                children: [
+                    '/advanced-fantasy/'
+                ]
+            }
+        ]
+    }
 }
