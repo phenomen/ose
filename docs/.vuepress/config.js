752db38860c30f3097718ed563e6d25daf6b4ddd
Aleksandr Vasilenko <phenomen@pm.me>
2019-08-29 07:20:35 -0500

    Zeit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  docsDir: 'docs',
+  lang: 'ru-RU',
+  title: 'OSE',
+  description: 'Версия на русском языке',
+  themeConfig: {
+    nav: [
+      { text: 'Основы', link: '/core/' },
+      { text: 'Фэнтези', link: '/fantasy/' },
+      { text: 'Магия', link: '/magic/' },
+      { text: 'Монстры', link: '/monsters/' },
+      { text: 'Сокровища', link: '/treasures/' }
+    ],
+    sidebar: [
+      {
+        title: 'Классические правила',
+        children: [
+      '/core/',
+      '/fantasy/',
+      '/magic/',
+      '/monsters/',
+      '/treasures/'
+                  ]
+       },
+       {
+         title: 'Расширенные правила',
+         children: [
+       '/advanced-fantasy/',
+       '/advanced-magic/'
+                   ]
+        }
+
+     ]
+  }
+}
