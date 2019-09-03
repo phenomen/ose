acbf149c8ded149e391376f3588c9baedfd4e5cb
Alexander Vasilenko <phenomen@pm.me>
2019-09-03 14:31:45 -0500

    Custom slugify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: 'Old-School Essentials',
     description: 'Версия на русском языке',
     themeConfig: {
-          sidebar: [{
+        sidebar: [{
                 title: 'Классическое фэнтези',
                 children: [
                     '/classic-fantasy/',
@@ -21,7 +21,19 @@ module.exports = {
         ]
     },
     markdown: {
-      anchor: { level: [1,2,3]
+        anchor: {
+            level: [1, 2, 3]
+        },
+        slugify: function(string) {
+            const rControl = /[\u0000-\u001f]/g
+            const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g
+
+            return string.toString().toLowerCase()
+                .replace(rControl, '')
+                .replace(rSpecial, '-')
+                .replace(/\-{2,}/g, '-')
+                .replace(/^\-+|\-+$/g, '')
+                .replace(/^(\d)/, '_$1')
+        }
     }
-  }
 }
