f067e064d7c951f63928a33a230454facf634d4b
phenomen <phenomen@pm.me>
2025-03-10 12:35:30 -0500

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,43 +4,43 @@ import starlight from "@astrojs/starlight";
 //import starlightMultiSidebar from "@lorenzo_lewis/starlight-multi-sidebar";
 
 import {
-	SidebarCharacters,
-	SidebarAdventures,
-	SidebarMonsters,
-	SidebarMagic,
-	SidebarTreasures,
-	SidebarExtras,
+  SidebarCharacters,
+  SidebarAdventures,
+  SidebarMonsters,
+  SidebarMagic,
+  SidebarTreasures,
+  SidebarExtras,
 } from "./sidebar.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ose.ruleplaying.com",
-	integrations: [
-		starlight({
-			plugins: [],
-			title: "Old-School Essentials",
-			social: {
-				github: "https://github.com/phenomen/ose",
-			},
-			defaultLocale: "root",
-			locales: {
-				root: {
-					label: "Русский",
-					lang: 'ru',
-				},
-			},
-			sidebar: [
-				SidebarCharacters,
-				SidebarAdventures,
-				SidebarMagic,
-				SidebarTreasures,
-				SidebarMonsters,
-				SidebarExtras,
-			],
-			customCss: [
-				"./src/styles/custom.css",
-				"@fontsource-variable/inter/index.css",
-			],
-		}),
-	],
+  site: "https://ose.ruleplaying.com",
+  integrations: [
+    starlight({
+      plugins: [],
+      title: "Old-School Essentials",
+      social: {
+        github: "https://github.com/phenomen/ose",
+      },
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "Русский",
+          lang: "ru",
+        },
+      },
+      sidebar: [
+        SidebarCharacters,
+        SidebarAdventures,
+        SidebarMagic,
+        SidebarTreasures,
+        SidebarMonsters,
+        SidebarExtras,
+      ],
+      customCss: [
+        "./src/styles/custom.css",
+        "@fontsource-variable/inter/index.css",
+      ],
+    }),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,18 @@ import { z, defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 const spellsCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		name: z.string(),
-		original: z.string(),
-		type: z.enum(["arcane", "divine"]),
-		level: z.number(),
-		duration: z.string(),
-		range: z.string(),
-	}),
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    original: z.string(),
+    type: z.enum(["arcane", "divine"]),
+    level: z.number(),
+    duration: z.string(),
+    range: z.string(),
+  }),
 });
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	spells: spellsCollection,
+  docs: defineCollection({ schema: docsSchema() }),
+  spells: spellsCollection,
 };
